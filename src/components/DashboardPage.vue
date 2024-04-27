d8065b27bbc42739ee36199e0f5198558d6c8924
Ahmad-Kurniawan <133091979+Ahmad-Kurniawan@users.noreply.github.com>
2024-04-27 15:28:37 +0700

    Add files via upload

---
+++ src/components/DashboardPage.vue
@@ -0,0 +1,234 @@
+<template>
+  <div>
+    <nav>
+      <div class="nav__content">
+        <div class="logo"><i><a href="#">INSEMTIF</a></i></div>
+        <label for="check" class="checkbox">
+          <i class="ri-menu-line"></i>
+        </label>
+        <input type="checkbox" name="check" id="check" />
+        <ul>
+          <li><router-link to="/dashboard">Dashbord</router-link></li>
+          <li><router-link to="/InputNilai">Input Nilai KP/TA</router-link></li>
+          <li><router-link to="/akunpage">Akun</router-link></li>
+          <li><router-link to="Logout">Keluar</router-link></li>
+        </ul>
+      </div>
+    </nav>
+    <section class="section">
+      <div class="section__container">
+        <div class="content">
+          <p class="subtitle">Selamat Datang Di,</p>
+          <h1 class="title">
+            <span style="color: black;">INSEMTIF<br/></span>
+          </h1>
+          <p class="description">
+            Website ini Dirancang Khusus Untuk Mempermudahkan Bapak/ibu Dalam Mengimputkan Nilai
+            Seminar Kerja Praktik (KP) Dan Nilai Seminar Tugas Akhir(TA)
+          </p>
+        </div>
+        <div class="image">
+          <img src="../assets/Gambar2.png" alt="">
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      isMenuOpen: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Open Sans', sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+nav {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #1d1f8d;
+  z-index: 99;
+}
+
+.nav__content {
+  max-width: 1200px;
+  margin: auto;
+  padding: 1.5rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+nav .logo a {
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #f9fafc;
+  transition: 0.3s;
+}
+
+nav .logo a:hover {
+  color: #fdfdff;
+}
+
+nav .checkbox {
+  display: none;
+}
+
+nav input {
+  display: none;
+}
+
+nav .checkbox i {
+  font-size: 2rem;
+  color: #f9fafc;
+  cursor: pointer;
+}
+
+ul {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  list-style: none;
+  transition: left 0.3s;
+  margin-right: 1rem; /* Menambahkan margin kanan agar ada ruang antara menu dengan ikon menu */
+}
+
+ul li {
+  margin-right: 1rem; /* Menambahkan margin kanan antara item menu */
+}
+
+ul li a {
+  padding: 0.5rem 1rem;
+  border: 2px solid transparent;
+  text-decoration: none;
+  font-weight: 600;
+  color: #ffffff;
+  transition: 0.5s;
+}
+
+ul li a:hover {
+  border-top-color: #fafafd;
+  border-bottom-color: #fafafd;
+  color: #fafafd;
+}
+
+.section {
+  background-color: #edf0f3;
+  padding-top: 4rem; /* Menambahkan padding atas agar konten tidak bertumpuk dengan navbar */
+}
+
+.section__container {
+  min-height: 100vh;
+  max-width: 1200px;
+  margin: auto;
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 4rem;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.subtitle {
+  letter-spacing: 2px;
+  color: #6b7280;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+}
+
+.title {
+  font-size: 2.5rem;
+  font-weight: 400;
+  line-height: 3rem;
+  color: #ffffff;
+  margin-bottom: 30px;
+}
+
+.title span {
+  font-weight: 800;
+}
+
+.description {
+  line-height: 1.5rem;
+  color: #6b7280;
+  margin-bottom: 2rem;
+}
+
+.image {
+  display: grid;
+  place-items: center;
+}
+
+.image img {
+  width: 430px;
+  height: 430px;
+  margin-right: 20px;
+}
+
+@media (max-width: 750px) {
+  nav .checkbox {
+    display: block;
+  }
+
+  ul {
+    position: absolute;
+    width: 100%;
+    height: calc(100vh - 85px);
+    left: -100%;
+    top: 85px;
+    background-color: #edf0f3;
+    flex-direction: column;
+    justify-content: center;
+    gap: 3rem;
+    padding-left: 0; 
+  }
+
+  nav #check:checked ~ ul {
+    left: 0;
+  }
+
+  ul li a {
+    font-size: 1.25rem;
+  }
+
+  .section__container {
+    padding: 10rem 1rem 5rem 1rem;
+    text-align: center;
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  .image {
+    grid-area: 1/1/2/2;
+  }
+
+  .action__btns {
+    margin: auto;
+  }
+}
+</style>
---
+++ src/components/RegisterPage.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-box">
+        <h1>Daftar</h1>
+
+        <label for="nama">Nama Lengkap</label>
+        <input type="text" id="nama" placeholder="Masukkan Nama Lengkap">
+
+        <label for="nip_nisn">NIP/NISN</label>
+        <input type="text" id="nip_nisn" placeholder="Masukkan NIP/NISN">
+
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Masukkan Email">
+
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Masukkan Password">
+
+        <label for="profesi">Profesi</label>
+        <select v-model="selectedProfesi" id="profesi">
+          <option value="">Pilih Profesi</option>
+          <option value="dosen_pembimbing">Dosen Pembimbing</option>
+          <option value="dosen_penguji">Dosen Penguji</option>
+        </select>
+        <button @click.prevent="register"><router-link to="/">Daftar</router-link></button>
+        <p style="text-align: center;">Sudah punya akun? <router-link to="/">Masuk</router-link></p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedProfesi: ''
+    };
+  },
+  methods: {
+    register() {
+
+      console.log('Proses registrasi...');
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #1d1f8d
+}
+
+.form-box {
+  background-color: #ffffff;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  width: 350px;
+  
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input, select {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  outline: none;
+}
+
+button {
+  background-color: #2468e6;
+  color: #fff;
+  padding: 14px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  margin: 0 auto;
+  display: flex;
+}
+
+button:hover {
+  background-color: #1e4f89;
+}
+
+a {
+  color: #ffffff;
+  text-decoration: none;
+}
+
+p a {
+  color: #2468e6; /* Warna teks biru */
+}
+
+</style>
